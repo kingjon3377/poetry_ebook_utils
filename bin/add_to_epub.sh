af453cbb5b05c9f135323e69710c673579572032
Jonathan Lovelace <kingjon3377@gmail.com>
2021-05-18 21:36:27 -0400

    add_to_epub.sh: Use exit, not return, since not in a function.

--- bin/add_to_epub.sh
+++ bin/add_to_epub.sh
@@ -7,7 +7,7 @@ files_to_include=( )
 for file in "$@";do
 	test -f "${file}" && files_to_include+=( "${file}" )
 done
-test "${#files_to_include[@]}" -eq 0 && return 0
+test "${#files_to_include[@]}" -eq 0 && exit 0
 tmpdir=$(mktemp -d)
 unzip -d "${tmpdir}" "${epub}"
 cp "${files_to_include[@]}" "${tmpdir}/EPUB"
