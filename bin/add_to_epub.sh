d3e3d273b4b5e95f3e7b1c008a01e993599b9746
Jonathan Lovelace <kingjon3377@gmail.com>
2022-10-24 22:59:57 -0400

    add_to_epub.sh: Refactor, adding debug output.

--- bin/add_to_epub.sh
+++ bin/add_to_epub.sh
@@ -3,27 +3,44 @@
 # the EPUB doesn't exist fails with an error, but silently succeeds if no files
 # to add provided (and silently discards any nonexistent files provided on the
 # command line)
+# TODO: Write test(s) of this script
+DEBUG_ON=false
 usage() {
-	echo "Usage: add_to_epub.sh ebook.epub file_to_add.ext [addl_file.ext ...]" 1>&2
+	echo "Usage: add_to_epub.sh [--debug|--no-debug] ebook.epub file_to_add.ext [addl_file.ext ...]" 1>&2
 }
-if ! test $# -ge 1; then
+debug_print() {
+	test "${DEBUG_ON:-false}" = true && echo "$@" 1>&2
+}
+
+epub=
+epub_defined=false
+files_to_include=( )
+for arg in "$@";do
+	if test "${arg}" = "--debug"; then
+		DEBUG_ON=true
+	elif test "${arg}" = "--no-debug"; then
+		DEBUG_ON=false
+	elif test "${epub_defined:-false}" != true -a -z "${epub}"; then
+		epub="${arg}"
+		epub_defined=true
+	elif test -f "${arg}"; then
+		files_to_include+=( "${arg}" )
+	else
+		debug_print "File ${arg} not found"
+	fi
+done
+if test "${epub_defined:-false}" != true -o -z "${epub}"; then
 	usage
 	exit 1
-fi
-epub="${1}"
-full_epub="$(realpath "${epub}")"
-shift
-if !  test -f "${epub}";
+elif !  test -f "${epub}";then
 	echo "EPUB file ${epub} does not exist" 1>&2
 	usage
 	exit 1
+elif test "${#files_to_include[@]}" -eq 0; then
+	debug_print "No files to add"
+	exit 0
 fi
-files_to_include=( )
-# TODO: support a "verbose debug" mode that logs if a specified file doesn't exist
-for file in "$@";do
-	test -f "${file}" && files_to_include+=( "${file}" )
-done
-test "${#files_to_include[@]}" -eq 0 && exit 0
+full_epub="$(realpath "${epub}")"
 tmpdir=$(mktemp -d)
 unzip -d "${tmpdir}" "${epub}"
 cp "${files_to_include[@]}" "${tmpdir}/EPUB"
