kingjon3377/poetry_ebook_utils

commit 597e4a290501522573f00bc0c4b9159b1edecc54

ebook_builder.sh, distrib.mk: Add support for 'pregen Markdown'
Currently only used for a case where the pregen LaTeX has an illustration between stanzas.
And the Makefile change here is only to ensure that the pregen LaTeX isn't overwritten by
converting the pregen Markdown.

bin/ebook_builder.sh
@@ -93,7 +93,13 @@ wrap_in_section() {
 
 # Include a poem into the ebook
 includepoem() {
-	if ! file_or_link "${1}" ; then
+	if file_or_link "${1}.pregen.md"; then
+		cat "${1}.pregen.md"
+		return
+	elif file_or_link "${1%%.md}.pregen.md"; then
+		cat "${1%%.md}.pregen.md"
+		return
+	else
 		echo "Poem ${1} not found" 1>&2
 		exit 2
 	fi

distrib.mk
@@ -108,7 +108,7 @@ $(ILLUST_DIR)/%.pdf: $(ILLUST_DIR)/%.svg
 # FIXME: The dependency on the pregen file if present doesn't seem to work, so
 # the user has to 'make clean' every time a pregen file is created, changed, or
 # removed.
-$(POEMS_DIR)/%.tex: $(POEMS_DIR)/%.md $(wildcard $(POEMS_DIR)/%.pregen.tex)
+$(POEMS_DIR)/%.tex: $(filter-out %.pregen.md,$(POEMS_DIR)/%.md) $($(wildcard $(POEMS_DIR)/%.pregen.tex))
 	@echo "Regenerating $@"
 	@$(md_to_tex_path) $(md_to_tex_arg) $(POEMS_DIR)/$*.md $@
 

commit 773aef90e66f727dc95a8e2208af86f2589baaa2

ebook_builder.sh: Fix breakage introduced in 597e4a290501522573f00bc0c4b9159b1edecc54

bin/ebook_builder.sh
@@ -99,7 +99,7 @@ includepoem() {
 	elif file_or_link "${1%%.md}.pregen.md"; then
 		cat "${1%%.md}.pregen.md"
 		return
-	else
+	elif ! file_or_link "${1}"; then
 		echo "Poem ${1} not found" 1>&2
 		exit 2
 	fi